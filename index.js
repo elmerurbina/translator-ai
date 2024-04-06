8ca8f0a6269a1f394fd0f01ab9dcbd8ae3406124
elmerurbina <[elmerurbina570@gmail.com]>
2024-04-06 00:09:03 -0600

    update

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-async function translateText() {
-    const text = document.getElementById('textInput').value;
-    const targetLanguage = document.getElementById('languageSelect').value;
-  
+async function translateText(text, targetLanguage) {
     try {
       const requestBody = { text, targetLanguage };
       const response = await fetch('https://translator-ai.elmerurbina570.workers.dev/', {
@@ -17,13 +14,14 @@ async function translateText() {
       }
   
       const responseData = await response.json();
-      document.getElementById('translatedText').innerText = responseData.translatedText;
+      return responseData.translatedText;
     } catch (error) {
       console.error('Translation error:', error);
-      document.getElementById('translatedText').innerText = 'Translation error. Please try again.';
+      return 'Translation error. Please try again.';
     }
   }
-  function startDictation() {
+  
+  async function startDictation() {
     if ('SpeechRecognition' in window || 'webkitSpeechRecognition' in window) {
       const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
       recognition.lang = document.getElementById('languageSelect').value; // Set language based on selected option
@@ -57,4 +55,35 @@ async function translateText() {
     }
   }
   
+  function handleFile(file) {
+    const reader = new FileReader();
+    reader.onload = async function(event) {
+      const text = event.target.result;
+      const targetLanguage = document.getElementById('languageSelect').value;
+  
+      // Perform translation
+      try {
+        const translatedText = await translateText(text, targetLanguage);
+        document.getElementById('translatedText').innerText = translatedText;
+      } catch (error) {
+        console.error('Translation error:', error);
+        document.getElementById('translatedText').innerText = 'Translation error. Please try again.';
+      }
+    };
+    reader.readAsText(file);
+  }
+  
+  async function translateTextInput() {
+    const text = document.getElementById('textInput').value;
+    const targetLanguage = document.getElementById('languageSelect').value;
+    
+    // Perform translation
+    try {
+      const translatedText = await translateText(text, targetLanguage);
+      document.getElementById('translatedText').innerText = translatedText;
+    } catch (error) {
+      console.error('Translation error:', error);
+      document.getElementById('translatedText').innerText = 'Translation error. Please try again.';
+    }
+  }
   
